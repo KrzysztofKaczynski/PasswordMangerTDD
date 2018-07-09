456f23ab27e39aaff6ef36533fe850d0ef3ea140
Krzysztof Kaczyński <qris70@gmail.com>
2018-07-09 09:59:10 +0200

    [add] method verify

--- PasswordManger/src/main/java/pl/example/pwdmanager/PasswordComplexityPolicy.java
+++ PasswordManger/src/main/java/pl/example/pwdmanager/PasswordComplexityPolicy.java
@@ -1,4 +1,25 @@
 package pl.example.pwdmanager;
 
 public class PasswordComplexityPolicy {
+
+    private static final int MIN_LENGHT = 8;
+    private static final int MIN_DIGITS = 3;
+
+    public boolean verify (String password){
+
+        int pwdLenght = password.length();
+        int digitCount = 0;
+        for (int i = 1; i < pwdLenght ; i++) {
+            if (Character.isDigit(password.charAt(i))){
+                digitCount++;
+            }
+        }
+        boolean result = true;
+        if (pwdLenght<MIN_LENGHT)
+            result = false;
+        if (digitCount<MIN_DIGITS)
+            result = false;
+        return result;
+
+    }
 }
