1872a93788a22cb03aa85b78a61ab1bae0fc3a7f
Krzysztof Kaczyński <qris70@gmail.com>
2018-07-09 10:53:54 +0200

    [add] PasswordComplexityPolicyTest

---
+++ PasswordManger/src/test/java/pl/example/pwdmanager/PasswordComplexityPolicyTest.java
@@ -0,0 +1,12 @@
+package pl.example.pwdmanager;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordComplexityPolicyTest {
+
+    @Test
+    public void testverify() {
+    }
+}
