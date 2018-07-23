bad7bc5a3e9af4783c2a50c9ede39d8fca76b274
Krzysztof Kaczyński <qris70@gmail.com>
2018-07-23 14:26:03 +0200

    [add] static final String

--- PasswordManger/src/test/java/pl/example/pwdmanager/PasswordComplexityPolicyTest.java
+++ PasswordManger/src/test/java/pl/example/pwdmanager/PasswordComplexityPolicyTest.java
@@ -6,16 +6,19 @@ import static org.junit.Assert.*;
 
 public class PasswordComplexityPolicyTest {
 
+    private static final String CORRECT_PASSWORD = "qwert123";
+    private static final String CORRECT_PASSWORD_DIGITS_FIRST = "123zxcvb";
+
     @Test
-    public void testverify() {
+    public void shouldBeCorrectForVerifyOnPasswordWith3Digits() {
         PasswordComplexityPolicy policyUnderTest = new PasswordComplexityPolicy();
-        boolean result = policyUnderTest.verify("qwert123");
+        boolean result = policyUnderTest.verify(CORRECT_PASSWORD);
         assertTrue("wynik dla qwert123", result);
     }
     @Test
-    public void testverify2() {
+    public void shouldBeCorrectForVerifyOnPasswordWith3DigitsOnBegining() {
         PasswordComplexityPolicy policyUnderTest = new PasswordComplexityPolicy();
-        boolean result = policyUnderTest.verify("123zxcvb");
+        boolean result = policyUnderTest.verify(CORRECT_PASSWORD_DIGITS_FIRST);
         assertTrue("wynik dla 123zxcvb", result);
     }
 }
