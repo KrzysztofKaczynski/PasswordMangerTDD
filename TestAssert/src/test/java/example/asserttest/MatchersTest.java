cd77259e033b1d868aa7854acf09e44b58cacd55
Krzysztof Kaczyński <qris70@gmail.com>
2018-07-26 14:54:24 +0200

    [add] MatchersTest and arryTest

--- TestAssert/src/test/java/example/asserttest/MatchersTest.java
+++ TestAssert/src/test/java/example/asserttest/MatchersTest.java
@@ -2,7 +2,10 @@ package example.asserttest;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class MatchersTest {
@@ -11,5 +14,43 @@ public class MatchersTest {
     public void testEqualTo() {
         String result = "wynik";
         assertThat(result, equalTo("wynik"));
+        assertThat(result, is("wynik"));
     }
+
+    @Test
+    public void testStrings() {
+        String result = "wynik bardzo skoplikowanej metody";
+        assertThat(result, containsString("bardzo"));
+        assertThat(result, startsWith("wynik"));
+        assertThat(result, endsWith("ody"));
+    }
+
+    @Test
+    public void testStringsAllOf() {
+        String result = "wynik bardzo skoplikowanej metody";
+        assertThat(result, allOf(containsString("bardzo"), endsWith("ody"), startsWith("wynik")));
+
+    }
+    @Test
+    public void testStringsAnyOf() {
+        String result = "wynik bardzo skoplikowanej metody";
+        assertThat(result, anyOf(containsString("bardzo"), endsWith("ady"), startsWith("wynik")));
+
+    }
+    @Test
+    public void testStringsComposed() {
+        String result = "wynik bardzo skoplikowanej metody";
+        assertThat(result, allOf(containsString("bardzo"),
+                anyOf(containsString("zzz"), endsWith("ody"), startsWith("aaa"))));
+
+    }
+    @Test
+    public void testArrays() {
+        List<String> result = Arrays.asList("element jeden", "element dwa", "element trzy");
+        assertThat(result, everyItem(startsWith("element")));
+        assertThat(result, hasItem("element dwa"));
+        assertThat(result, hasItem(endsWith("dwa")));
+
+    }
+
 }
